e34cb7e1efa71ba13c7de109f77fc28dce7dc461
cthulhu-F <admaxelfranco@protonmail.com>
2026-03-19 15:24:16 -0300

    desbloquear csp para mastercard.com

--- header.php
+++ header.php
@@ -30,10 +30,6 @@ $topbarData = $resultTopbar && $resultTopbar->num_rows > 0 ? $resultTopbar->fetc
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <meta http-equiv="Content-Security-Policy" content="
-  default-src 'self';
-  script-src 'self' https://sandbox.src.mastercard.com 'unsafe-inline' 'unsafe-eval';
-">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilos.css?v=19">
