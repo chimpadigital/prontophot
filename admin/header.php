e37300effe21a9eae33c28d0168317616eb53b2b
cthulhu-F <admaxelfranco@protonmail.com>
2026-04-08 14:31:15 -0300

    fix: boton cerrar sesion

--- admin/header.php
+++ admin/header.php
@@ -91,7 +91,7 @@
                 console.log("salir")
                 e.preventDefault();
                 $.post('../inc/salir.php', function(data) {
-                    //window.location.reload();
+                    window.location.href = "/";
                 });
             });
         });
