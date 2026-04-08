3a6e1bfe0c0ddabb5999adabf66ee97f3e6eb7f6
cthulhu-F <admaxelfranco@protonmail.com>
2026-04-08 14:17:05 -0300

    fix: boton cerrar sesion

--- admin/header.php
+++ admin/header.php
@@ -7,15 +7,15 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilos.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- GOOGLE FONTS -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
 
     <link href="node_modules\hamburgers\dist\hamburgers.css" rel="stylesheet">
-    
-	
-	
+
+
+
     <title>Prontophot</title>
 </head>
 
@@ -40,15 +40,14 @@
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-dark">
                 <a class="navbar-brand" href="index.php"><img class="logo-blanco responsive" src="assets\img\logo-pronto-white.svg" alt=""></a>
-                <a class="nav-link active"><button type="button" class="btn btn-bg-white btn-sm cerrarSesion">Salir</button></a>
-                
+
                 <button class="hamburger hamburger--collapse" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
                     <span class="hamburger-box mt-1">
                         <span class="hamburger-inner"></span>
                     </span>
-                </button>            
+                </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
@@ -60,8 +59,12 @@
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="cupones.php">Cupones de Descuento</a>
-                        </li>                      
-                    </ul>                    
+                        </li>
+                        <li class="nav-link active text-medium">
+                            <button type="button" class="p-0 bg-transparent text-white border-0 text-medium cerrarSesion">Cerrar Sesión <i class="fa fa-sign-out" aria-hidden="true"></i>
+                            </button>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </div>
@@ -73,14 +76,15 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
     <script src="js/printThis.js"></script>
     <script>
-    // Look for .hamburger
-    var hamburger = document.querySelector(".hamburger");
-    // On click
-    hamburger.addEventListener("click", function() {
-        // Toggle class "is-active"
-        hamburger.classList.toggle("is-active");
-        // Do something else, like open/close menu
-    });
+        // Look for .hamburger
+        var hamburger = document.querySelector(".hamburger");
+        // On click
+        hamburger.addEventListener("click", function() {
+            // Toggle class "is-active"
+            hamburger.classList.toggle("is-active");
+            // Do something else, like open/close menu
+        });
     </script>
-    </body>
+</body>
+
 </html>
