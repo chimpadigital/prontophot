472a807842c24476b66604bf38420d61aab83a01
cthulhu-F <admaxelfranco@protonmail.com>
2026-04-08 14:51:05 -0300

    fix: mobile - correcciones de nav bar

--- header.php
+++ header.php
@@ -422,7 +422,7 @@ $topbarData = $resultTopbar && $resultTopbar->num_rows > 0 ? $resultTopbar->fetc
         <div class="container">
             <nav class="navbar navbar-mobile navbar-expand-lg navbar-dark">
                 <a class="navbar-brand" href="index.php"><img class="logo-blanco responsive" src="assets/img/logo-pronto-white.svg" alt=""></a>
-                <li class="nav-item">
+                <li class="nav-item" style="list-style: none;">
                     <a id="carrito-tienda" class="nav-link" href="mi-pedido"><button type="button" class="btn text-white btn-cart position-relative pr-0"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
                                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                             </svg><span id="cartContador" class="badge badge-dark">0</span></button></a>
