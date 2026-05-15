14fa24db7df6b2763740ab588d0f15893f777895
cthulhu-F <admaxelfranco@protonmail.com>
2026-05-15 13:28:27 -0300

    fix: status de pedido en misPedidos

--- usuario/detallePedido.php
+++ usuario/detallePedido.php
@@ -17,7 +17,7 @@ $rowf = $facturacion->fetch_assoc();
 ?>
 <div class="container-fluid bg-black border-top border-white">
     <div class="row">
-        <div class="col-4 bg-black py-4 px-3 d-none d-md-block">
+        <div class="col-2 bg-black py-4 px-3 d-none d-md-block">
             <div class="align-items-end d-flex flex-column justify-items-end mt-3 mb-4">
                 <a href="/"><img class="logo-blanco" src="../assets/img/logo-pronto-white.svg" alt=""></a>
             </div>
@@ -41,7 +41,7 @@ $rowf = $facturacion->fetch_assoc();
         </div>
         <!-- FIN COL-4  -->
 
-        <div class="col-md-8 bg-white p-5 rounded-lg columna-content-admin">
+        <div class="col-md-10 bg-white p-5 rounded-lg columna-content-admin">
 
             <!-- tab-content -->
             <div class="tab-content" id="v-pills-tabContent">
